cd1046717d05cebb3d17d311435fbe890d82289b
Mikebrown4768 <52458720+Mikebrown4768@users.noreply.github.com>
2019-07-24 13:25:07 -0400

    index.html, todo.js (1:21pm) 7/24/19
    Added comprehensive comments, added an organizer section. (Non intractable just yet.) Need code to be able to add the to-dos to those and a way to display what is stored in the organizer.

--- todo.js
+++ todo.js
@@ -31,15 +31,15 @@
     // Create a new list item when clicking on the "Add" button
     function newElement() {
       var li = document.createElement("li");
-      var inputValue = document.getElementById("myInput").value;
+      var inputValue = document.getElementById("UserInput").value;
       var t = document.createTextNode(inputValue);
       li.appendChild(t);
       if (inputValue === '') {
         alert("You must write something!");
       } else {
-        document.getElementById("myUL").appendChild(li);
+        document.getElementById("User's list").appendChild(li);
       }
-      document.getElementById("myInput").value = "";
+      document.getElementById("UserInput").value = "";
     
       var span = document.createElement("SPAN");
       var txt = document.createTextNode("\u00D7");
@@ -52,5 +52,32 @@
           var div = this.parentElement;
           div.style.display = "none";
         }
+        
+//Code for organizer in Progress Feel free to change it but state the changes -->
+
+    function Myfunction() {
+            var li = document.createElement("li");
+            var inputValue = document.getElementById("User's Organizer").value;
+            var t = document.createTextNode(inputValue);
+            li.appendChild(t);
+            if (inputValue === '') {
+              alert("You must write something!");
+            } else {
+              document.getElementById("User's Organizer").appendChild(li);
+            }
+            document.getElementById("UserInput").value = "";
+          
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            li.appendChild(span);
+          
+            for (i = 0; i < close.length; i++) {
+              close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
+              }
+            
       }
     }
